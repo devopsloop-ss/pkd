85d4c6c2e4f39a9601e5b94df96292f468c27be2
sos4aug <sos4aug@LAPTOP-U7OL3CR5.localdomain>
2020-01-25 00:47:50 +0530

    first commit

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "profile"{
+    type = string
+    default = "default"
+}
+
+variable "credentials"{
+    type = string
+    default = "$HOME/.aws/credentials"
+}
